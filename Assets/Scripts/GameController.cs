c159eddc0eb29be27cbfba3a5764817d26b979bb
maxatcodeninjas <zunixs12@gmail.com>
2024-08-01 16:47:42 -0500

    ok

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject platform;
+    float pos = 0;
+    
+    public GameObject gameOverCanvas;
+    
+    void Start()
+    {
+        for (int i = 0; i < 1000; i++){
+            SpawnPlatforms();
+        }
+    }
+
+    void SpawnPlatforms(){
+        Instantiate(platform, new Vector3(Random.value * 10 - 5f, pos, 0.5f), Quaternion.identity);
+
+        pos += 2.5f;
+    }
+
+    public void GameOver(){
+        gameOverCanvas.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControls : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Rigidbody2D rb;
+
+    public float downSpeed = 20f;
+
+    public float movementSpeed = 10f;
+
+    public float movement = 0f;
+
+    public Text scoreText;
+
+    private float topScore = 0.0f;
+
+    public void OnCollisionEnter2D(Collision2D collision){
+        Vector2 velocity = rb.velocity;
+
+        if (velocity.y <= 0){
+            rb.velocity = new Vector3(rb.velocity.x, downSpeed, 0);
+        }
+
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal") * movementSpeed;
+
+        if (movement < 0){
+            this.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else{
+            this.GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        if (rb.velocity.y > 0 && transform.position.y > topScore){
+            topScore = transform.position.y;
+        }
+        scoreText.text = "Score: " + Mathf.Round(topScore).ToString();
+    }
+}
